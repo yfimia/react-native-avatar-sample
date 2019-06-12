d901ae1d65fa7a81c6f81582bcbeab63e8960bc9
Yoilán Fimia León <yfimia@gmail.com>
2019-06-12 18:22:46 -0400

    First commit

---
+++ App.js
@@ -0,0 +1,210 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import PhotoUpload from 'react-native-photo-upload'
+import ImageResizer from 'react-native-image-resizer'
+import { Row, Col, Button } from 'native-base'
+
+const PHOTO_SIZE_LARGE = 170;
+const PHOTO_SIZE_MEDIUM = 100;
+const PHOTO_SIZE_SMALL = 70;
+
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            largePhotoUri: 'https://i.pinimg.com/474x/52/fe/c5/52fec54732ea8fa383f614f447aec4ac--avatar-james-cameron-blue-avatar.jpg',
+            mediumPhotoUri: 'https://i.pinimg.com/474x/52/fe/c5/52fec54732ea8fa383f614f447aec4ac--avatar-james-cameron-blue-avatar.jpg',
+            smallPhotoUri: 'https://i.pinimg.com/474x/52/fe/c5/52fec54732ea8fa383f614f447aec4ac--avatar-james-cameron-blue-avatar.jpg',
+            photoUri : 'https://i.pinimg.com/474x/52/fe/c5/52fec54732ea8fa383f614f447aec4ac--avatar-james-cameron-blue-avatar.jpg',
+
+            offsetX : 0,
+            offsetY : 0,
+            width : PHOTO_SIZE_LARGE,
+            height : PHOTO_SIZE_LARGE,
+            percent : 100,
+            displayWidth : PHOTO_SIZE_LARGE,
+            displayHeight : PHOTO_SIZE_LARGE,
+            resizeMode : 'cover'
+
+        }
+    }
+
+    changePhoto(avatar, x){
+        console.log('Image base64 string: ', avatar);
+        console.log('Otro: ', x);
+        this.setState({ photoUri: `data:image/png;base64,${avatar}` });
+        
+    }
+
+    generatePreview(){
+        console.log(this.state.photoUri)
+        //this.forceUpdate()
+    }
+
+    zoomIn(){
+        this.setState({displayWidth : this.state.percent + 1})
+    }
+
+    zommOut(){
+        this.setState({displayWidth : this.state.percent - 1})
+    }
+
+    processResponse(response){
+        console.log("Response: ", response)
+    }
+
+    render() {
+        // 
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.container}>
+
+                    <Row style={styles.imagePickerRow}>
+                        <Col style={styles.pickerCol}>
+                            <Text style={styles.selectPictureText}>Select Picture</Text>
+                            <PhotoUpload 
+                                onPhotoSelect={ (avatar, x) => this.changePhoto(avatar, x) }  
+                                onResponse={ (response) => this.processResponse(response) }
+                            >
+                                <Image
+                                    style={styles.pickerImg}
+                                    resizeMode='cover'
+                                    source={{ uri: this.state.photoUri }}
+                                />
+                            </PhotoUpload>
+                            
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><Button block onPress={() => this.zoomIn() }><Text>+</Text></Button></Col>
+                        <Col><Button block onPress={() => this.zommOut() }><Text>-</Text></Button></Col>
+                        <Col><Button block onPress={() => this.zoomIn() }><Text>Rotate Left</Text></Button></Col>
+                        <Col><Button block onPress={() => this.zommOut() }><Text>Rotate Right</Text></Button></Col>
+                    </Row>
+                    <Row>
+                        <Col><Button block onPress={() => this.zoomIn() }><Text>Move Left</Text></Button></Col>
+                        <Col><Button block onPress={() => this.zommOut() }><Text>Move Right</Text></Button></Col>
+                        <Col><Button block onPress={() => this.zoomIn() }><Text>Move Up</Text></Button></Col>
+                        <Col><Button block onPress={() => this.zommOut() }><Text>Move Down</Text></Button></Col>
+                    </Row>
+
+                    {/* <Row>
+                        <Col><Button block onPress={() => this.generatePreview() }><Text>Preview</Text></Button></Col>
+                    </Row> */}
+
+
+                    <Row style={styles.previewRow}>
+                        <Col style={styles.previewLargeCol}>
+                            <Image
+                                style={styles.largeAvatarImg}
+                                resizeMode='cover'
+                                source={{ uri: this.state.photoUri }}
+                            />
+                        </Col>
+                        <Col style={styles.previewMediumCol}>
+                            <Image
+                                style={styles.mediumAvatarImg}
+                                resizeMode='cover'
+                                source={{ uri: this.state.photoUri }}
+                            />
+                        </Col>
+                        <Col style={styles.previewSmallCol}>
+                            <Image
+                                style={styles.smallAvatarImg}
+                                resizeMode='cover'
+                                source={{ uri: this.state.photoUri }}
+                            />
+                        </Col>
+                    </Row>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    imagePickerRow: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        height: 380,
+    },
+    pickerCol: {
+        margin: 15,
+        // backgroundColor: '#FF0000',
+    },
+    pickerImg: {
+        paddingVertical: 0,
+        width: 250,
+        height: 250,
+        borderRadius: 0
+    },
+    selectPictureText:{
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+
+    previewRow: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        height: PHOTO_SIZE_LARGE,
+    },
+
+    previewLargeCol: {
+        // backgroundColor: '#FF0000',
+        width: PHOTO_SIZE_LARGE,
+        margin: 3
+    },
+
+    previewMediumCol: {
+        // backgroundColor: '#00FF00',
+        width: PHOTO_SIZE_MEDIUM,
+        margin: 3
+    },
+
+    previewSmallCol: {
+        // backgroundColor: '#0000FF',
+        width: PHOTO_SIZE_SMALL,
+        margin: 3
+    },
+
+    largeAvatarImg: {
+        height: PHOTO_SIZE_LARGE,
+        width: PHOTO_SIZE_LARGE,
+        // borderColor: '#000000',
+        // borderWidth: 1,
+        borderRadius: PHOTO_SIZE_LARGE / 2
+    },
+    mediumAvatarImg: {
+        height: PHOTO_SIZE_MEDIUM,
+        width: PHOTO_SIZE_MEDIUM,
+        // borderColor: '#000000',
+        // borderWidth: 1,
+        borderRadius: PHOTO_SIZE_MEDIUM / 2
+    },
+    smallAvatarImg: {
+        height: PHOTO_SIZE_SMALL,
+        width: PHOTO_SIZE_SMALL,
+        // borderColor: '#000000',
+        // borderWidth: 1,
+        borderRadius: PHOTO_SIZE_SMALL / 2
+    },
+
+
+
+
+
+
+
+});
